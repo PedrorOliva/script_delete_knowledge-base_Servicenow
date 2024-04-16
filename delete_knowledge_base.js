754013222197e2cd445cae29cb6c96edb029e8c3
Pedro Oliveira <pedro.robson@zup.com.br>
2024-04-16 00:13:31 -0300

    Update delete_knowledge_base.js

--- delete_knowledge_base.js
+++ delete_knowledge_base.js
@@ -1,6 +1,8 @@
-var kbase = new GlideRecord('kb_knowledge_base');  
-kbase.addQuery('sys_id', 'coloque o sys_id da base aqui');   // clique com o botão direito em cima do nome da base e copie o sys_id
-kbase.query();   
+var kbase = new GlideRecord('kb_knowledge_base');  // Inicializa um objeto GlideRecord para interagir com a tabela de base de conhecimento
+kbase.addQuery('sys_id', 'coloque o sys_id da base aqui');   // Adiciona um critério de busca para encontrar a base de conhecimento pelo seu sys_id.
+kbase.query();   // Executa a busca na tabela com o critério especificado.
+// Itera sobre os resultados da busca. Para cada base de conhecimento encontrada:
 while (kbase.next()) {  
+    // Deleta a base de conhecimento atual.
     kbase.deleteRecord();  
-} 
+}
